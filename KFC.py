cf8e173c49c92feb02ebdfb2bc6af3c4e034cebd
prajith15 <prajithkumarshanmugam@gmail.com>
2025-08-06 01:40:03 -0700

    success

---
+++ KFC.py
@@ -0,0 +1,8 @@
+day=input("enter a day:")
+food=("chickenburger");
+amount=599;
+if(amount>=599)and(food=="chickenburger")and(day in["sunday","wednesday"]):
+    print("you are eligible for 70% discount")
+else:
+    print("you are not eligible for 70% discount")
+    
